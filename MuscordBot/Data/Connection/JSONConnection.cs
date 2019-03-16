6b720312cee4d7d0f303898ae7e8509946a8224d
Wannes De Craene <wannes.decraene.y0550@student.hogent.be>
2019-03-16 11:04:18 +0100

    [feat] Added option to use JSON

---
+++ MuscordBot/Data/Connection/JSONConnection.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace MuscordBot.Data.Connection
+{
+	public class JSONConnection
+	{
+		public string Link { get; set; }
+		public JSONConnection(string link)
+		{
+			Link = link;
+		}
+
+		public JObject ApiCall()
+		{
+			try
+			{
+				WebClient client = new WebClient();
+				Stream stream = client.OpenRead(Link);
+				StreamReader reader = new StreamReader(stream);
+
+				JObject jObject = JObject.Parse(reader.ReadLine());
+
+				stream.Close();
+
+				return jObject;
+
+			}
+			catch (WebException e) when ((e.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.InternalServerError)
+			{
+				return (JObject)"Something went wrong...";
+			}
+		}
+	}
+}
