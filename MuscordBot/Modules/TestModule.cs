bd0e38b131266c3940506e11287cf2613c26d3ac
michiel-schoofs <michiel.schoofs@student.hogent.be>
2019-03-16 10:44:41 +0100

    Added bot init + ping command

---
+++ MuscordBot/Modules/TestModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscordBot.Modules {
+    [Name("Commands for testing purposes")]
+    public class TestModule: ModuleBase<SocketCommandContext> {
+
+        //kleine letters voor commando naam
+        [Command("ping")]
+        [Alias("p")]//alias voor het commando
+        public async Task respondPing() {
+            await Context.Channel.SendMessageAsync("pong");
+        }
+    }
+}
--- MuscordBot/Program.cs
+++ MuscordBot/Program.cs
@@ -1,9 +1,72 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace MuscordBot {
     class Program {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private readonly string prefix = "!";
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync().GetAwaiter().GetResult();
         }
+
+        public async Task MainAsync() {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            
+            await InstallCommands();
+
+            DependencyInjection(); 
+
+
+            Console.WriteLine("Logging in...");
+            string botKey = "NTU2NDAyNzc3NTg5NTQ3MDA4.D25O0Q.GQaVOA5qo_lb42m04LMUyrplLVQ";
+            await _client.LoginAsync(TokenType.Bot,botKey);
+            await _client.StartAsync();
+
+            Console.WriteLine("Ready");
+
+            await Task.Delay(-1);
+        }
+
+        private void DependencyInjection() {
+            Console.WriteLine("Injecting to database");
+
+            _services = new ServiceCollection()
+                            .AddSingleton(_client)
+                            .BuildServiceProvider();
+        }
+
+        public async Task InstallCommands() {
+            _client.MessageReceived += HandleCommands;
+
+            Console.WriteLine("Initializing commands...");
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+
+        private async Task HandleCommands(SocketMessage arg) {
+            
+            if (arg == null || arg.Author.IsBot)
+                return;
+
+            SocketUserMessage msg = arg as SocketUserMessage;
+            int argPos = 0;
+
+            if (msg.HasStringPrefix(prefix, ref argPos) || msg.HasMentionPrefix(_client.CurrentUser, ref argPos)) {
+                var context = new SocketCommandContext(_client, msg);
+                IResult result = await _commands.ExecuteAsync(context, argPos, _services);
+            }
+
+        }
+
     }
 }
